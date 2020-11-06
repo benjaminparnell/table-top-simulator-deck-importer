e59311a0662c4a1321ff4e98891f4e1e35c2ebaa
Benjamin Parnell <benjaminparnell.94@gmail.com>
2020-11-06 11:56:21 +0000

    Refactor

--- src/DeckImportModal/View.elm
+++ src/DeckImportModal/View.elm
@@ -24,9 +24,11 @@ import Css
         , translate2
         , width
         , zIndex
+        , int
+        , rgba
         )
 import DeckImportModal.Model
-import Html.Styled exposing (Html, div, styled)
+import Html.Styled exposing (Html, div, styled, text, p, textarea)
 import Html.Styled.Attributes exposing (rows)
 import Html.Styled.Events exposing (onClick, onInput)
 import Msg exposing (Msg)
@@ -41,8 +43,8 @@ view model =
         , left (px 0)
         , width (pct 100)
         , height (pct 100)
-        , backgroundColor (Css.rgba 0 0 0 0.3)
-        , zIndex (Css.int 100)
+        , backgroundColor (rgba 0 0 0 0.3)
+        , zIndex (int 100)
         ]
         []
         [ styled div
@@ -57,14 +59,14 @@ view model =
             , borderRadius (px 5)
             , displayFlex
             , flexDirection column
-            , boxShadow4 (px 1) (px 1) (px 5) (Css.rgba 0 0 0 0.5)
-            , zIndex (Css.int 100)
+            , boxShadow4 (px 1) (px 1) (px 5) (rgba 0 0 0 0.5)
+            , zIndex (int 100)
             ]
             []
-            [ Html.Styled.p []
-                [ Html.Styled.text "Paste deck list" ]
-            , Html.Styled.textarea [ rows 20, onInput Msg.UpdateDeckString ] []
-            , UI.button [ marginTop (px 20) ] [ onClick (Msg.ImportDeck model.deckString) ] [ Html.Styled.text "Add cards to deck" ]
-            , UI.button [ marginTop (px 10) ] [ onClick Msg.ToggleModal ] [ Html.Styled.text "Close" ]
+            [ p []
+                [ text "Paste deck list" ]
+            , textarea [ rows 20, onInput Msg.UpdateDeckString ] []
+            , UI.button [ marginTop (px 20) ] [ onClick (Msg.ImportDeck model.deckString) ] [ text "Add cards to deck" ]
+            , UI.button [ marginTop (px 10) ] [ onClick Msg.ToggleModal ] [ text "Close" ]
             ]
         ]
